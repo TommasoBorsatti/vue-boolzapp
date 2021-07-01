17c60d49a2f7015bcb85d71f8ff253fca543c9ef
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-07-01 20:28:10 +0200

    Aggiunte Feature Extra + Migliorie grafiche
    Sto cercando di rendere il più somigliante possibile l'interfaccia simulando il comportamento di hover e button; in più ho aggiunto la feature di cancellazione dei messaggi con menu a tendina dinamico. Sto aggiungendo lo stesso tipo di menu anche alla colonna dei contatti.

--- js/script.js
+++ js/script.js
@@ -7,112 +7,206 @@ const App = new Vue (
 
     data: {
 
-      newTxt: [],
-      searchTxt: "",
-      contactIndex: 0,
-      bubbleColor: 'sent',
       rubrica:[
         {
           nome: "Mino Ciccini",
           avatar: "img/avatar_1.jpg",
           accessTime: "09:20",
           visible: true,
+          selected: false,
+          active: false,
           msg:[
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/21",
+              time: "15:30",
               text: 'Hai portato a spasso il cane?',
-              status: 'sent'
+              status: 'sent',
+              active: false
             },
             {
-              date: '10/01/2020 15:50:00',
+              date: '10/01/21',
+              time: '15:50',
               text: 'Ricordati di dargli da mangiare',
-              status: 'sent'
+              status: 'sent',
+              active: false
             },
             {
-              date: '10/01/2020 16:15:22',
+              date: '10/01/21',
+              time: '16:15',
               text: 'Tutto fatto!',
-              status: 'received'
+              status: 'received',
+              active: false
             },
           ],
         },
         {
-          nome: "Larderello Bohr",
+          nome: "Roberto Esperto",
           avatar: "img/avatar_2.jpg",
           accessTime: "10:40",
           visible: true,
+          selected: false,
+          active: false,
           msg:[
             {
-              date: '20/03/2020 16:30:00',
+              date: '20/03/21',
+              time: '16:30',
               text: 'Ciao come stai?',
-              status: 'sent'
+              status: 'sent',
+              active: false
             },
             {
-              date: '20/03/2020 16:30:55',
+              date: '20/03/21',
+              time: '16:32',
               text: 'Bene grazie! Stasera ci vediamo?',
-              status: 'received'
+              status: 'received',
+              active: false
             },
             {
-              date: '20/03/2020 16:35:00',
+              date: '20/03/21',
+              time: '16:39',
               text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-              status: 'sent'
+              status: 'sent',
+              active: false
             },
           ],
         },
         {
-          nome: "Pigiamino Morbidozzi",
+          nome: "Pino Tomino",
           avatar: "img/avatar_3.jpg",
           accessTime: "04:20",
           visible: true,
+          selected: false,
+          active: false,
           msg:[
             {
-              date: '28/03/2020 10:10:40',
+              date: '28/03/21',
+              time: '10:10',
               text: 'La Marianna va in campagna',
-              status: 'received'
+              status: 'received',
+              active: false
             },
             {
-              date: '28/03/2020 10:20:10',
+              date: '28/03/21',
+              time: '10:20',
               text: 'Sicuro di non aver sbagliato chat?',
-              status: 'sent'
+              status: 'sent',
+              active: false
             },
             {
-              date: '28/03/2020 16:15:22',
+              date: '28/03/21',
+              time: '16:15',
               text: 'Ah scusa!',
-              status: 'received'
+              status: 'received',
+              active: false
             },
           ],
         },
         {
-          nome: "King Rum",
+          nome: "Bruno Pruno",
           avatar: "img/avatar_4.jpg",
           accessTime: "11:10",
           visible: true,
+          selected: false,
+          active: false,
           msg:[
             {
-              date: '10/01/2020 15:30:55',
+              date: '10/01/21',
+              time: '15:30',
               text: 'Lo sai che ha aperto una nuova pizzeria?',
-              status: 'sent'
+              status: 'sent',
+              active: false
             },
             {
-              date: '10/01/2020 15:50:00',
+              date: '10/01/21',
+              time: '15:50',
               text: 'Si, ma preferirei andare al cinema',
-              status: 'received'
+              status: 'received',
+              active: false
+            },
+          ],
+        },
+        {
+          nome: "Fiona Campiona",
+          avatar: "img/avatar_6.jpg",
+          accessTime: "17:10",
+          visible: true,
+          selected: false,
+          active: false,
+          msg:[
+            {
+              date: '10/01/21',
+              time: '15:30',
+              text: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent',
+              active: false
+            },
+            {
+              date: '10/01/21',
+              time: '15:50',
+              text: 'Si, ma preferirei andare al cinema',
+              status: 'received',
+              active: false
+            },
+          ],
+        },
+        {
+          nome: "Lapo il Capo",
+          avatar: "img/avatar_5.jpg",
+          accessTime: "17:10",
+          visible: true,
+          selected: false,
+          active: false,
+          msg:[
+            {
+              date: '10/01/21',
+              time: '15:30',
+              text: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent',
+              active: false
+            },
+            {
+              date: '10/01/21',
+              time: '15:50',
+              text: 'Si, ma preferirei andare al cinema',
+              status: 'received',
+              active: false
             },
           ],
         },
       ],
+
+      newTxt: [],
+      searchTxt: "",
+      contactIndex: 0,
+      contactActionIndex: 0,
+      messageIndex:0,
+      bubbleColor: 'sent',
+      
 
     },
 
     methods:{
 
       chooseContact: function(i){
+
+        this.rubrica[this.contactIndex].msg.forEach(message => {
+          return message.active = false;
+        });
+
+        this.rubrica.forEach(contact => {
+          contact.selected = false;
+        });
+
         this.contactIndex = i;
+        this.rubrica[this.contactIndex].selected = !this.selected;
+        
       },
 
       addMsg: function(){
         if (this.newTxt != "") {
           this.rubrica[this.contactIndex].msg.push({
-            date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+            date: dayjs().format('DD/MM/YY'),
+            time: dayjs().format('HH:mm'),
             text: this.newTxt,
             status: "sent",
           });
@@ -124,8 +218,9 @@ const App = new Vue (
 
       autoreply: function(){
         this.rubrica[this.contactIndex].msg.push({
-          date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-          text: "Ok!",
+          date: dayjs().format('DD/MM/YY'),
+          time: dayjs().format('HH:mm'),
+          text: "Ok! A dopo..",
           status: "received",
         });
 
@@ -151,7 +246,47 @@ const App = new Vue (
           }
       },
 
+      callToContact: function(i){
+        this.contactActionIndex = i; 
+
+        if (this.rubrica[this.contactActionIndex].active == false){
+          this.rubrica.forEach(contact => {
+            return contact.active = false;
+          });
+          return this.rubrica[this.contactActionIndex].active = true;
+        } else {
+          this.rubrica.forEach(contact => {
+            return contact.active = false;
+          });
+        }
+      
+      },
+
+      callToMsg: function(i){
+        this.messageIndex = i; 
+
+        if (this.rubrica[this.contactIndex].msg[this.messageIndex].active == false){
+          this.rubrica[this.contactIndex].msg.forEach(message => {
+            return message.active = false;
+          });
+          return this.rubrica[this.contactIndex].msg[this.messageIndex].active = true;
+        } else {
+          this.rubrica[this.contactIndex].msg.forEach(message => {
+            return message.active = false;
+          });
+        }
+      
+      },
+
+      deleteMessage: function(i){
+        this.rubrica[this.contactIndex].msg[this.messageIndex].active = false;
+        console.log(this.rubrica[this.contactIndex].msg[this.messageIndex].active);
+        this.rubrica[this.contactIndex].msg.splice(this.messageIndex, 1);
+    
+        
+      }
+
     },
 
-  }
+  }   
 );
